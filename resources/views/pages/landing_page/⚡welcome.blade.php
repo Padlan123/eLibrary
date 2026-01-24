7e8f763933980f6664988aa0f36ea972a1855f0c
lion <waedi189@gmail.com>
2026-01-24 13:09:24 +0700

    menambahkan halaman welcome dan membuat component navbar untuk landing_page

---
+++ resources/views/pages/landing_page/⚡welcome.blade.php
@@ -0,0 +1,50 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component {
+    public function render()
+    {
+        return $this->view()->title('Welcome');
+    }
+};
+?>
+
+<div>
+    <div class="bg-gray-900">
+        @livewire('landing_page.navbar')
+
+        <div class="relative isolate px-6 pt-10 lg:px-8">
+            <div aria-hidden="true"
+                class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+                <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                    class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75">
+                </div>
+            </div>
+            <div class="mx-auto max-w-2xl py-32 sm:py-48">
+
+                <div class="text-center">
+                    <h1 class="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">MEMBACA BUKU
+                        DIMANA SAJA</h1>
+                    <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">E-Book adalah situs baca
+                        Buku online, baca Buku materi pelajaran, Baca Buku Cerita, baca Self-development dalam bahasa
+                        indonesia. Tampa iklan mengganggu dan hanya di E-Book</p>
+                    <div class="mt-10 flex items-center justify-center gap-x-6">
+                        <a href="#"
+                            class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Daftar
+                            Sekarang</a>
+                        <a href="{{ route('fitur') }}" class="text-sm/6 font-semibold text-white">Pelajari Lebih Lanjut
+                            <span aria-hidden="true">→</span></a>
+                    </div>
+                </div>
+            </div>
+            <div aria-hidden="true"
+                class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+                <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                    class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::livewire('/', 'pages::landing_page.welcome')->name('welcome');
+Route::livewire('/fitur', 'pages::landing_page.fitur')->name('fitur');
+Route::livewire('/beranda', 'pages::pelanggan.beranda');
