7e04d5c088d21fd8730fd4e64586cb9162df674c
lion <waedi189@gmail.com>
2026-01-27 20:01:45 +0700

    menambahkan migrasi untuk roles

---
+++ database/migrations/2026_01_27_124854_roles.php
@@ -0,0 +1,27 @@
+<?php
+
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'pelanggan']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Role::where(['name' => 'admin'])->delete();
+        Role::where(['name' => 'pelanggan'])->delete();
+    }
+};
