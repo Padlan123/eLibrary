0ae2e53d49ce98445c18ab33436f484c6ace1258
lion <waedi189@gmail.com>
2026-01-31 21:13:44 +0700

    membuat halaman register

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/login');
 });
-Route::livewire('/login', 'pages::auth.login');
+Route::livewire('/login', 'pages::auth.login')->name('login');
+Route::livewire('/register', 'pages::auth.register')->name('register');
